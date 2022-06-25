391f17ad825fbb42bd75d3e0e6efa789b96c41b5
Jacob Tews <jacob.tews@cnu.edu>
2022-06-25 10:48:42 -0400

    cleaned disposition codes (hooray)

--- preprocessing.py
+++ preprocessing.py
@@ -18,6 +18,52 @@ def event_number_clean(num):
     return int(num[2:])
 
 
+def disposition_code_clean(row):
+    
+    # The three variables calculated here must all be strings for the function to work.
+    
+    # First test for nan (the disposition code or tencode is a float if it is nan, otherwise it's a str).
+    if isinstance(row['Disposition Code'], float):
+        return row
+    else:
+        disp_code_letter = str(row['Disposition Code'])[-1:]
+        disp_code_number = str(row['Disposition Code'])[:-1]
+
+    if isinstance(row['Tencode Suffix'], float):
+        tencode_suffix = ''
+    else:
+        tencode_suffix = str(row['Tencode Suffix'])
+    
+    if disp_code_letter.isnumeric():
+        return row
+    elif disp_code_letter not in ['A', 'C', 'O', 'P']:
+        if disp_code_letter not in tencode_suffix:
+            row['Tencode Suffix'] = tencode_suffix + disp_code_letter
+        else:
+            return row    
+    else:
+        if disp_code_letter == 'A':
+            if disp_code_number in ['1', '4', '5', '7', '8', '9', '10', '11', '13', '14', '15']:
+                if disp_code_letter not in tencode_suffix:
+                    row['Tencode Suffix'] = tencode_suffix + disp_code_letter
+                    row['Disposition Code'] = disp_code_number
+                else:
+                    row['Disposition Code'] = disp_code_number
+            else:
+                return row
+        elif disp_code_letter in ['C', 'O', 'P']:
+            if disp_code_letter not in tencode_suffix:
+                row['Tencode Suffix'] = tencode_suffix + disp_code_letter
+            else:
+                return row
+    
+    # After all the above processing, if there is still nothing in the tencode suffix column, return it to nan
+    if row['Tencode Suffix'] == '':
+        row['Tencode Suffix'] = np.nan
+    
+    return row
+
+
 def preprocess():
     pass
     
